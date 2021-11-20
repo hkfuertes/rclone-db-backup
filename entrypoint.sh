2b9d64d3b40d5a17ed48779c7e37a42130b1e2a2
Miguel Fuertes <hkfuertes@gmail.com>
2021-11-20 17:34:24 +0100

    changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ cat <<EOF >> /backup.sh
 #!/bin/bash
 echo "[\`date +"%d-%b-%Y %T"\`] Backing up ${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}..."
 /usr/bin/mysqldump --host ${DATABASE_HOST} --port ${DATABASE_PORT} -u ${DATABASE_USER} -p${DATABASE_PASSWORD} -y ${DATABASE_NAME} > /tmp/backup_${DATABASE_NAME}_latest.sql
-cd /tmp; zip /tmp/backup_${DATABASE_NAME}_latest.sql.zip /tmp/backup_${DATABASE_NAME}_latest.sql; cd /
+cd /tmp; zip backup_${DATABASE_NAME}_latest.sql.zip backup_${DATABASE_NAME}_latest.sql; cd /
 cp /tmp/backup_${DATABASE_NAME}_latest.sql.zip /tmp/backup_${DATABASE_NAME}_\`date +"%Y%m%d"\`.sql.zip
 echo "[\`date +"%d-%b-%Y %T"\`] Copying to ${REMOTE_SERVICE}:${REMOTE_FOLDER}..."
 /usr/bin/rclone copy /tmp/ ${REMOTE_SERVICE}:${REMOTE_FOLDER} --include "*.sql.zip"
